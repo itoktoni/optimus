804123b2d7c8ca65ba603fc375e31a180e5da64e
Itok toni laksono <itok.toni@gmail.com>
2021-03-28 23:02:19 +0700

    linen : fix linen description

--- Modules/Item/Resources/views/page/linen/form.blade.php
+++ Modules/Item/Resources/views/page/linen/form.blade.php
@@ -32,6 +32,6 @@
 <div class="form-group">
     {!! Form::label('name', 'Description', ['class' => 'col-md-2 control-label']) !!}
     <div class="col-md-10">
-        {!! Form::textarea('item_category_description', null, ['class' => 'form-control', 'rows' => '3']) !!}
+        {!! Form::textarea('item_linen_description', null, ['class' => 'form-control', 'rows' => '3']) !!}
     </div>
 </div>
