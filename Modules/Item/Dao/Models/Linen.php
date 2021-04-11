48892be4fd169e9f0dabc37f106f7ebaee9434e0
Itok toni laksono <itok.toni@gmail.com>
2021-04-11 20:22:21 +0700

    update

--- Modules/Item/Dao/Models/Linen.php
+++ Modules/Item/Dao/Models/Linen.php
@@ -63,7 +63,7 @@ class Linen extends Model
     public $searching = 'item_linen_rfid';
     public $datatable = [
         'item_linen_id' => [true => 'Code', 'width' => 50],
-        'item_linen_rfid' => [true => 'RFID', 'width' => 150],
+        'item_linen_rfid' => [true => 'RFID', 'width' => 200],
         'item_linen_product_id' => [false => 'Product Id'],
         'item_product_name' => [true => 'Product Name'],
         'item_linen_location_id' => [false => 'Location Id'],
